a2f95d8859267ce6732f1059bb29d52d575eeb31
Jaeha Yi <jaehaha1@gmail.com>
2023-11-06 19:58:49 -0800

    created battlemanager and VR camera working, need to get VR hand working

---
+++ Assets/Scripts/BattleManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleManager : MonoBehaviour
+{
+    private int usership_count = 0;
+    private int enemyship_count = 0;
+
+    public enum Team
+    {
+        user,
+        enemy
+    };
+
+    public enum ShipType
+    {
+        regular
+    };
+
+    [SerializeField]
+    private GameObject[] userShips;
+
+    [SerializeField]
+    private GameObject[] enemyShips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SpawnShip(Team team, ShipType shipType, Vector3 location)
+    {
+        if (team == Team.user)
+        {
+            Instantiate(userShips[(int)shipType], location, Quaternion.identity);
+            usership_count++;
+        }
+        if (team == Team.enemy)
+        {
+            Instantiate(enemyShips[(int)shipType], location, Quaternion.identity);
+            enemyship_count++; 
+        }
+    }
+
+    private void CheckWin()
+    {
+        if (usership_count == 0 || enemyship_count == 0)
+        {
+            Debug.Log("Game ended."); 
+        }
+    }
+}
